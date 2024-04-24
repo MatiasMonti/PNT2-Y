fcc158d4edda3c912d332d188b283af2d610bd87
Mati Monti <matiasmonti@hotmail.com>
2024-04-24 14:51:33 -0300

    commit clase 5 vue

---
+++ MisArchivos/clase5/src/components/Saludo/Saludo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1> clase 5 </h1>
+        <h3> componentes vue </h3>
+        <h4> holas {{mensaje}} </h4>
+    </div>
+</template>
+
+<script>
+    export default{
+        props:{
+            mensaje : String
+        },
+        data(){
+
+        },
+    }
+</script>
+
+<style lang="">
+    
+</style>
+
+
+
